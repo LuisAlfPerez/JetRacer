a23e0276f6b218eea0e0ab0f7b14b1e55519e668
LuisAlfPerez <33231415+LuisAlfPerez@users.noreply.github.com>
2022-01-11 01:54:18 -0600

    Update laneRecognition.py

--- LaneRecognition/laneRecognition.py
+++ LaneRecognition/laneRecognition.py
@@ -1,1 +1,12 @@
+import nanocamera
+import cv2
+camera = nanocamera.Camera(flip=0, width=1280, height=800, fps=30)
+#frame = camera.read()
+while(True):
+	ret, frame = camera.read()
+	cv2.imshow('frame', frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+    	break
 
+camera.release()
+cv2.destrolAllWindows()
