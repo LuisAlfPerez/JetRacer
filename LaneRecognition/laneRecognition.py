19f965e27df16015a359b5d079ed98eae3a79a07
LuisAlfPerez <33231415+LuisAlfPerez@users.noreply.github.com>
2022-01-11 01:59:50 -0600

    Update laneRecognition.py

--- LaneRecognition/laneRecognition.py
+++ LaneRecognition/laneRecognition.py
@@ -30,7 +30,7 @@ camera = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
 #frame = camera.read()
 while(True):
 	ret, frame = camera.read()
-	cv2.imshow('frame', frame)
+	cv2.imshow('JetRacer Camera', frame)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
     		break
 camera.release()
