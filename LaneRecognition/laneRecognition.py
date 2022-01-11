c0335bebb7e6102ce62e0223f983af778ec78d14
LuisAlfPerez <33231415+LuisAlfPerez@users.noreply.github.com>
2022-01-11 03:19:43 -0600

    Update laneRecognition.py

--- LaneRecognition/laneRecognition.py
+++ LaneRecognition/laneRecognition.py
@@ -46,7 +46,7 @@ if (True):
 	threshMean = cv2.adaptiveThreshold(blurredMean, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 25, 8)
 	threshGaussian = cv2.adaptiveThreshold(blurredGaussian,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,49,8)
 	kernelErosion = numpy.ones((3,3),numpy.uint8)
-	erosionMean = cv2.erode(threshMean,kernelErosion,iterations = 1)
+	erosionMean = cv2.erode(threshMean,kernelErosion,iterations = 3)
 	erosionGaussian = cv2.erode(threshGaussian,kernelErosion,iterations = 1)
 	kernelErosion = numpy.ones((7,7),numpy.uint8)
 	dilateMean = cv2.dilate(erosionMean,kernelErosion,iterations = 1)
