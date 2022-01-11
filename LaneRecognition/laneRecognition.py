64cd090d58dae8db50b7248add773383561978a9
LuisAlfPerez <33231415+LuisAlfPerez@users.noreply.github.com>
2022-01-11 02:36:29 -0600

    Update laneRecognition.py

--- LaneRecognition/laneRecognition.py
+++ LaneRecognition/laneRecognition.py
@@ -56,7 +56,7 @@ if (True):
 	    plt.xticks([]),plt.yticks([])
 	plt.show()
 	#cv2.imshow('JetRacer Camera', plt)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-    		break
+	#if cv2.waitKey(1) & 0xFF == ord('q'):
+    		#break
 camera.release()
 cv2.destroyAllWindows()
