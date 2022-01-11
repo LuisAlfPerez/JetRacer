856bf296d6e0d7d4b563bee000ea5e08209226ba
LuisAlfPerez <33231415+LuisAlfPerez@users.noreply.github.com>
2022-01-11 03:36:17 -0600

    Update laneRecognition.py

--- LaneRecognition/laneRecognition.py
+++ LaneRecognition/laneRecognition.py
@@ -55,8 +55,8 @@ if (True):
 	openingMean = cv2.morphologyEx(threshMean, cv2.MORPH_OPEN, kernelOpening)
 	openingGaussian = cv2.morphologyEx(threshGaussian, cv2.MORPH_OPEN, kernelOpening)
 	titles = ['Original Image', 'Gray',
-            'GaussianBlur', 'Adaptive Mean Thresholding','Erosion Mean','Dilate Mean','Opening Mean', 'GaussianBlur', 'Adaptive Gaussian Thresholding','Erosion Gaussian','Dilate Gaussian','Opening Gaussian']
-	images = [frame, gray, blurredMean,threshMean, erosionMean, dilateMean, openingMean, blurredGaussian, threshGaussian, erosionGaussian, dilateGaussian, openingGaussian]
+            'GaussianBlur', 'GaussianBlur', 'Adaptive Mean Thresholding','Erosion Mean','Dilate Mean','Opening Mean', 'Adaptive Gaussian Thresholding','Erosion Gaussian','Dilate Gaussian','Opening Gaussian']
+	images = [frame, gray, blurredMean, blurredGaussian, threshMean, erosionMean, dilateMean, openingMean, threshGaussian, erosionGaussian, dilateGaussian, openingGaussian]
 	plt.figure('JetRacer Camera')
 	for i in range(12):
 	    plt.subplot(3,4,i+1),plt.imshow(images[i],'gray')
