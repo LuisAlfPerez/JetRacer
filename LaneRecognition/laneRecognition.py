5da3eb7c1a867ebca15b98a9863df1fafee4f562
LuisAlfPerez <33231415+LuisAlfPerez@users.noreply.github.com>
2022-01-11 02:19:27 -0600

    Update laneRecognition.py

--- LaneRecognition/laneRecognition.py
+++ LaneRecognition/laneRecognition.py
@@ -1,4 +1,5 @@
-import nanocamera
+from matplotlib import pyplot as plt
+import numpy
 import cv2
 def gstreamer_pipeline(
     capture_width=1280,
@@ -26,11 +27,33 @@ def gstreamer_pipeline(
             display_height,
         )
     )
+def gallery(array, ncols=3):
+    nindex, height, width, intensity = array.shape
+    nrows = nindex//ncols
+    assert nindex == nrows*ncols
+    # want result.shape = (height*nrows, width*ncols, intensity)
+    result = (array.reshape(nrows, ncols, height, width, intensity)
+              .swapaxes(1,2)
+              .reshape(height*nrows, width*ncols, intensity))
+    return result
 camera = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
 #frame = camera.read()
 while(True):
 	ret, frame = camera.read()
-	cv2.imshow('JetRacer Camera', frame)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+	threshMean = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 10)
+	threshGaussian = cv.adaptiveThreshold(blurred,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,11,2)
+	openingMean = cv.morphologyEx(threshMean, cv.MORPH_OPEN, kernel)
+	openingGaussian = cv.morphologyEx(threshGaussian, cv.MORPH_OPEN, kernel)
+	titles = ['Original Image', 'Gray',
+            'GaussianBlur', 'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding','Opening Mean','Opening Gaussian']
+	images = [frame, gray, blurred, threshMean, threshGaussian, openingMean, openingGaussian]
+	for i in range(7):
+	    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+	    plt.title(titles[i])
+	    plt.xticks([]),plt.yticks([])
+	cv2.imshow('JetRacer Camera', plt)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
     		break
 camera.release()
