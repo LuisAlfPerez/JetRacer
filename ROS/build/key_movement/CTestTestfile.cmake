8fbf2d3e94c94db1b8a280a1692975c66bb5f6c1
LuisAlfPerez <33231415+LuisAlfPerez@users.noreply.github.com>
2022-01-24 14:13:37 -0600

    Delete ROS directory

--- ROS/build/key_movement/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luisalfredo/JetRacer/ROS/src/key_movement
-# Build directory: /home/luisalfredo/JetRacer/ROS/build/key_movement
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
