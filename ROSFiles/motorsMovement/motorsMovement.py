dc872aa18a834b7502918d0b83597726b17c81e5
LuisAlfPerez <33231415+LuisAlfPerez@users.noreply.github.com>
2022-03-07 02:01:04 -0600

    Update motorsMovement.py

--- ROSFiles/motorsMovement/motorsMovement.py
+++ ROSFiles/motorsMovement/motorsMovement.py
@@ -5,23 +5,24 @@ import rospy
 from std_msgs.msg import Float32
 
 def moveSteer(message):
-	car.steering = float(message)/100
-	rospy.loginfo("Steering: %s", message)
+	rospy.loginfo("Steering: %d", message.data)
+	car.steering = (message.data)
 
 def moveMotor(message):
-	car.throttle = float(message)/100
-	rospy.loginfo("Throttle: %s", message)
+	car.throttle = (message.data)
+	rospy.loginfo("Throttle: %d", message.data)
 
 def listener():
 	rospy.init_node('motors', anonymous=True)
 	rospy.Subscriber("steering", Float32, moveSteer)
 	rospy.Subscriber("motor", Float32, moveMotor)
+	rospy.spin()
+	rospy.loginfo("Iniciando3")	
 
-
-listener()
 car = NvidiaRacecar()
 car.steering = 0
 car.steering_gain = 0.6
 car.steering_offset = 0
 car.throttle = 0
 car.throttle_gain = 1
+listener()
