dd5092cea995f74a84fecdd210d658d6f8f58ea1
Luis Alfredo <a01329646@tec.mx>
2022-04-07 13:59:40 -0500

    ROS little mistake :D

--- ROSFiles/LaneRecognition/control.py
+++ ROSFiles/LaneRecognition/control.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from setup.nvidia_racecar import NvidiaRacecar
 import rospy
 from std_msgs.msg import Int32
+from std_msgs.msg import Float32
 
 publisherMotor = None
 publisherSteering = None
