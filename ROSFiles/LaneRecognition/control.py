4aad7d7cb4ca6e72a1fc17e583ecf3d4550d35b4
Luis Alfredo <a01329646@tec.mx>
2022-04-07 13:58:26 -0500

    ROS changes indent

--- ROSFiles/LaneRecognition/control.py
+++ ROSFiles/LaneRecognition/control.py
@@ -11,27 +11,27 @@ def determineSteerValue(message):
 	global publisherMotor
 	global publisherSteering
 	rospy.loginfo("Steering: %d", message.data)
-    steering = message.data / 25
-    motor = 0.3
-    if steering > 1:
-    	steering = 1
-    if steering < -1:
-    	steering = -1
-    if motor > 1:
-    	motor = 1
-    publisherSteering.publish(float(steering))
-    publisherMotor.publish(float(motor))
+	steering = message.data / 25
+	motor = 0.3
+	if steering > 1:
+		steering = 1
+	if steering < -1:
+		steering = -1
+	if motor > 1:
+		motor = 1
+	publisherSteering.publish(float(steering))
+	publisherMotor.publish(float(motor))
 	
 def listener():
 	global publisherMotor
 	global publisherSteering
 	publisherMotor = rospy.Publisher('motor', Float32, queue_size=1)
-    publisherSteering = rospy.Publisher('steering', Float32, queue_size=1)
+	publisherSteering = rospy.Publisher('steering', Float32, queue_size=1)
 	rospy.init_node('control', anonymous=True)
 	rospy.Subscriber("referenceDistance", Int32, determineSteerValue)
 	r = rospy.Rate(10) 
-    while not rospy.is_shutdown:
-        r.sleep()
-    rospy.spin()
+	while not rospy.is_shutdown:
+	    r.sleep()
+	rospy.spin()
 if __name__ == '__main__':
 	listener()
