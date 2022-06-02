275b1e5194a8934bea9ef38cbad4f4c06976586f
Luis Alfredo <a01329646@tec.mx>
2022-06-01 20:29:33 -0500

    IoT modified

--- ROSFiles/LaneRecognition/iotMultithread.py
+++ ROSFiles/LaneRecognition/iotMultithread.py
@@ -74,7 +74,7 @@ def control(error):
     currentError = error
     
     k_proportional = 1/75
-    k_derivative = 1/750    
+    k_derivative = 1/500    
     
     derivative = (currentError - thirdError)
     
