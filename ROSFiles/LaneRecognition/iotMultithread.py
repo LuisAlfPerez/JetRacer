6b21867e0cc229f74ba3e8e21f56cb81e1a5ee11
Luis Alfredo <a01329646@tec.mx>
2022-06-02 21:35:24 -0500

    IoT modified

--- ROSFiles/LaneRecognition/iotMultithread.py
+++ ROSFiles/LaneRecognition/iotMultithread.py
@@ -84,7 +84,7 @@ def control(error):
     derivative = (currentError - thirdError)
     
     steering = k_proportional*currentError + k_derivative * derivative
-    motor = -0.17
+    motor = -0.14
     motors_movement(motor, steering)
     return
 
